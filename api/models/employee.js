0d350d1848924d05e04167f7e03cb31af8d537f6
faraz66 <46417303+faraz66@users.noreply.github.com>
2020-09-16 12:07:56 +0530

    Add files via upload

--- api/models/employee.js
+++ api/models/employee.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
+// this is the entry which will go in  the database so design it carefully 
+const employeeSchema  = mongoose.Schema({
+    id : mongoose.Schema.Types.ObjectId,
+    name : String,
+    designation : String,
+    salary: Number,
+    email :{type : String, required: true,unique: true,match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])/},
+    number : Number
 
+});
+
+module.exports = mongoose.model('Employee',employeeSchema);
