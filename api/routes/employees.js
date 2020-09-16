75d4df72f124755a89620fdf6a25684f5989ab24
faraz66 <46417303+faraz66@users.noreply.github.com>
2020-09-16 12:08:18 +0530

    Add files via upload

--- api/routes/employees.js
+++ api/routes/employees.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const router = express.Router();     
+const multer =require('multer');
+const EmployeeController =require("../controllers/employees");
 
+//Calling the api - get request
+router.get("/",EmployeeController.employees_get_all);
+//calling the api -creating new employee
+router.post("/",EmployeeController.create_employee);
+//calling the api -finding employee by id
+router.get("/:EmployeeId",EmployeeController.employees_get_employee);
+// patching request
+router.patch("/:EmployeeId",EmployeeController.employees_update_employee);
+// delete request 
+router.delete("/:EmployeeId",EmployeeController.employees_delete_employee);
+//Using router as Class Name To access The instance
+module.exports = router;
